deb24c7a8db9ffeea752b75586042767c80f53cb
AROY <AROY@AROY>
2020-05-08 12:30:54 +0530

    Commit for Exception Handling

---
+++ GroovyBasics/src/com/groovy/basic/tutorials/ExceptionHandlingBasics.groovy
@@ -0,0 +1,22 @@
+package com.groovy.basic.tutorials
+
+/**
+ * Exception handling is the same as Java.
+ */
+
+/*
+ * ARM Try with resources
+ */
+
+/**
+ * Power assertion
+ */
+// assert [left expression] == [right expression] : (optional message)
+// assert 1+1 == 3
+//Will yield:
+//
+//Caught: Assertion failed:
+//
+//assert 1+1 == 3
+//        |  |
+//        2  false
